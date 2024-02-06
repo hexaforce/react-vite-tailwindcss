c4aa645ad4efea9f9b16d24d779b41f3fbbdddf4
tantaka <tomokazu.tantaka@gmail.com>
2024-02-06 23:46:27 +0900

    update

--- src/views/Description.jsx
+++ src/views/Description.jsx
@@ -2,7 +2,7 @@ import { PaperClipIcon } from '@heroicons/react/20/solid'
 
 export function Description() {
   return (
-    <div>
+    <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
       <div className='px-4 sm:px-0'>
         <h3 className='text-base font-semibold leading-7 text-gray-900'>Applicant Information</h3>
         <p className='mt-1 max-w-2xl text-sm leading-6 text-gray-500'>Personal details and application.</p>
