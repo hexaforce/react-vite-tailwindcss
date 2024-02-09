cfbed66baecbd1c96d99045d36803763afa2bbf6
tantaka <tomokazu.tantaka@gmail.com>
2024-02-09 22:00:14 +0900

    update

--- src/components/cloudinary/index.jsx
+++ src/components/cloudinary/index.jsx
@@ -9,17 +9,15 @@ export default function CloudinaryImage() {
   const c = new Cloudinary({
     cloud: {
       cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
-      apiKey: import.meta.env.VITE_CLOUDINARY_API_KEY,
-      apiSecret: import.meta.env.VITE_CLOUDINARY_API_SECRET,
+      // apiKey: import.meta.env.VITE_CLOUDINARY_API_KEY,
+      // apiSecret: import.meta.env.VITE_CLOUDINARY_API_SECRET,
     },
   })
 
   return (
     <div className='App-body'>
-      <div>
-        <AdvancedImage cldImg={c.image('test/f9fnmvpmrttdrty1ub6o').resize(fill().width(200).height(300))} />
-        {/* <AdvancedImage cldImg={myImage} plugins={[lazyload(), responsive(100), placeholder()]} /> */}
-      </div>
+      <AdvancedImage cldImg={c.image('test/f9fnmvpmrttdrty1ub6o').resize(fill().width(200).height(300))} />
+      {/* <AdvancedImage cldImg={myImage} plugins={[lazyload(), responsive(100), placeholder()]} /> */}
     </div>
   )
 }
