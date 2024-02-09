0dc81b907afc81779dfca02d767fcbdfd6554546
tantaka <tomokazu.tantaka@gmail.com>
2024-02-09 18:22:36 +0900

    update

--- src/components/cloudinary/index.jsx
+++ src/components/cloudinary/index.jsx
@@ -1,11 +1,12 @@
 import './index.css'
 import { Cloudinary } from '@cloudinary/url-gen'
-import { AdvancedImage, AdvancedVideo, accessibility, responsive, lazyload, placeholder } from '@cloudinary/react'
+import {
+  AdvancedImage, // AdvancedVideo, accessibility, responsive, lazyload, placeholder
+} from '@cloudinary/react'
 import { fill } from '@cloudinary/url-gen/actions/resize'
 
 export default function CloudinaryImage() {
-
-  const cld = new Cloudinary({
+  const c = new Cloudinary({
     cloud: {
       cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
       apiKey: import.meta.env.VITE_CLOUDINARY_API_KEY,
@@ -13,14 +14,10 @@ export default function CloudinaryImage() {
     },
   })
 
-  const myImage = cld.image('test/f9fnmvpmrttdrty1ub6o')
-
-  myImage.resize(fill().width(200).height(300))
-
   return (
     <div className='App-body'>
       <div>
-        <AdvancedImage cldImg={myImage} />
+        <AdvancedImage cldImg={c.image('test/f9fnmvpmrttdrty1ub6o').resize(fill().width(200).height(300))} />
         {/* <AdvancedImage cldImg={myImage} plugins={[lazyload(), responsive(100), placeholder()]} /> */}
       </div>
     </div>
