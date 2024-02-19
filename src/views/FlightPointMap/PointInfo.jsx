00eb891fa7e485d398b442cf7835eb31599a9bfe
tantaka <tomokazu.tantaka@gmail.com>
2024-02-19 14:56:29 +0900

    update

--- src/views/FlightPointMap/PointInfo.jsx
+++ src/views/FlightPointMap/PointInfo.jsx
@@ -33,6 +33,7 @@ export default function PointInfo(props) {
         <div className='mt-2 flex items-center text-sm text-gray-500'>
           <MapPinIcon className='mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400' aria-hidden='true' />
           緯度:{markerInfo ? markerInfo.latitude : '---' }
+          <br />
           軽度:{markerInfo ? markerInfo.longitude : '---' }
         </div>
         {!editMode && (
