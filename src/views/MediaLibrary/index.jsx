37cafd47476fab4b7308d27aefcb1e4b937bfd7c
tantaka <tomokazu.tantaka@gmail.com>
2024-02-19 21:18:27 +0900

    update

--- src/views/MediaLibrary/index.jsx
+++ src/views/MediaLibrary/index.jsx
@@ -11,24 +11,24 @@ const products = [
     price: '$35',
     color: 'Black',
   },
-  // {
-  //   id: 2,
-  //   name: 'Basic Tee',
-  //   href: '#',
-  //   imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-  //   imageAlt: "Front of men's Basic Tee in black.",
-  //   price: '$35',
-  //   color: 'Black',
-  // },
-  // {
-  //   id: 3,
-  //   name: 'Basic Tee',
-  //   href: '#',
-  //   imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-  //   imageAlt: "Front of men's Basic Tee in black.",
-  //   price: '$35',
-  //   color: 'Black',
-  // },
+  {
+    id: 2,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 3,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
   // {
   //   id: 4,
   //   name: 'Basic Tee',
@@ -59,17 +59,18 @@ export default function MediaLibrary() {
 
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error: {error.message}</div>
-  console.log(data)
+  console.log(data?.Contents)
 
   return (
     <div className='bg-white'>
-      <div className='mx-auto max-w-2xl px-4 py-8 sm:px-2 sm:py-4 lg:max-w-7xl lg:px-8'>
+      <div className='mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8'>
         <h2 className='text-2xl font-bold tracking-tight text-gray-900'>Customers also purchased</h2>
+
         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
           {products.map((product) => (
             <div key={product.id} className='group relative'>
-              <div className='aspect-h-1 aspect-w-1 w-60 overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-40'>
-                <img src={product.imageSrc} alt={product.imageAlt} className='h-40 w-60 object-cover object-center lg:h-40 lg:w-60' />
+              <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80'>
+                <img src={product.imageSrc} alt={product.imageAlt} className='h-full w-full object-cover object-center lg:h-full lg:w-full' />
               </div>
               <div className='mt-4 flex justify-between'>
                 <div>
@@ -88,13 +89,13 @@ export default function MediaLibrary() {
         </div>
       </div>
 
-      <div className='mx-auto max-w-2xl px-4 py-8 sm:px-2 sm:py-4 lg:max-w-7xl lg:px-8'>
+      <div className='mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8'>
         <h2 className='text-2xl font-bold tracking-tight text-gray-900'>Customers also purchased</h2>
         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
           {products.map((product) => (
             <div key={product.id} className='group relative'>
-              <div className='aspect-h-1 aspect-w-1 w-60 overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-40'>
-                <img src={product.imageSrc} alt={product.imageAlt} className='h-40 w-60 object-cover object-center lg:h-40 lg:w-60' />
+              <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80'>
+                <img src={product.imageSrc} alt={product.imageAlt} className='h-full w-full object-cover object-center lg:h-full lg:w-full' />
               </div>
               <div className='mt-4 flex justify-between'>
                 <div>
