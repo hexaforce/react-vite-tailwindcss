f41113ae9c12e7806a67dd5a018bf5a8c4f891f9
tantaka <tomokazu.tantaka@gmail.com>
2024-02-21 10:38:10 +0900

    udate

--- src/views/FlightPointMap/PointForm.jsx
+++ src/views/FlightPointMap/PointForm.jsx
@@ -1,144 +1,175 @@
-import { Fragment, useRef } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { CameraIcon, PhotoIcon } from '@heroicons/react/24/solid'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { PhotoIcon, CameraIcon } from '@heroicons/react/24/solid'
+
 import PropTypes from 'prop-types'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useMutation } from '@apollo/client'
 import { CREATE_FLIGHT_POINT_MUTATION } from '@/queries/FlightPoint'
 import { uploadFileToS3 } from '@/queries/FileUpload'
 
-PointForm.propTypes = {
-  open: PropTypes.bool.isRequired,
-  setOpen: PropTypes.func.isRequired,
-  formData: PropTypes.shape({
-    latitude: PropTypes.number,
-    longitude: PropTypes.number,
-    title: PropTypes.string,
-    markerImage: PropTypes.instanceOf(Blob),
-  }).isRequired,
-  setFormData: PropTypes.func.isRequired,
+const initialFormValue = {
+  latitude: 0.0,
+  longitude: 0.0,
+  title: '',
+  markerImage: null,
 }
 
-export default function PointForm(props) {
+PointFormInput.propTypes = {
+  setOpenPointForm: PropTypes.func.isRequired,
+  selectPoint: PropTypes.shape({
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+  }),
+}
 
-  const { open, setOpen, formData, setFormData } = props
+export function PointFormInput({ setOpenPointForm, selectPoint }) {
+  const [formData, setFormData] = useState({...initialFormValue})
 
-  const cancelButtonRef = useRef(null)
+  useEffect(() => {
+    if (!selectPoint) return
+    setFormData(prevFormData => ({ ...prevFormData, ...selectPoint }));
+  }, [selectPoint])
 
-  const inputChange = (event) => {
+  function inputChange(event) {
     const { name, value } = event.target
     setFormData({ ...formData, [name]: value })
   }
 
   function fileInputChange(event) {
-    setFormData({ ...formData, markerImage: event.target.files[0] })
+    const { name, files } = event.target
+    setFormData({ ...formData, [name]: files[0] })
   }
 
   const { getIdTokenClaims } = useAuth0()
   const [createFlightPoint] = useMutation(CREATE_FLIGHT_POINT_MUTATION)
-  const onSubmit = async (event) => {
+
+  async function submit(event) {
     event.preventDefault()
+
     const token = (await getIdTokenClaims()).__raw
     const data = await uploadFileToS3(token, 'fpv-japan-public', formData.markerImage)
+    
     const createFlightPointInput = {
       latitude: formData.latitude,
       longitude: formData.longitude,
       title: formData.title,
       marker_image: data.fileKey,
     }
+
     try {
       createFlightPoint({ variables: { createFlightPointInput } })
-        .then((response) => console.log('response:', response.data))
+        .then(() => {
+          setOpenPointForm(false)
+          setFormData(initialFormValue)
+        })
         .catch((error) => console.log('error:', error))
-      setOpen(false)
     } catch (error) {
       console.error(error.message)
     }
   }
 
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as='div' className='relative z-10' initialFocus={cancelButtonRef} onClose={setOpen}>
-        <Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0'>
+    <form action='#' method='POST' className='mx-auto max-w-xl' onSubmit={submit}>
+      <div className='border-b border-gray-900/10 pb-12'>
+        <div className='mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+          <div className='col-span-full'>
+            <label htmlFor='title' className='block text-sm font-medium leading-6 text-gray-900'>
+              タイトル
+            </label>
+            <div className='mt-2'>
+              <input type='text' name='title' value={formData.title} onChange={inputChange} className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
+            </div>
+          </div>
+
+          <div className='col-span-full'>
+            <label className='block text-sm font-medium leading-6 text-gray-900'>
+              マーカー画像
+              {formData.markerImage ? <img src={URL.createObjectURL(formData.markerImage)} alt={formData.markerImage.name} className='img-preview sticky h-12 w-12 rounded-full bg-fixed object-cover' /> : <CameraIcon className='h-12 w-12 text-gray-300' aria-hidden='true' />}
+            </label>
+            <div className='mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10'>
+              <div className='text-center'>
+                <PhotoIcon className='mx-auto h-12 w-12 text-gray-300' aria-hidden='true' />
+                <div className='mt-4 flex justify-center items-center text-sm leading-6 text-gray-600'>
+                  <label htmlFor='markerImage' className='relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500'>
+                    <span>画像ファイルをアップロード</span>
+                    <input type='file' id='markerImage' name='markerImage' onChange={fileInputChange} className='sr-only' />
+                  </label>
+                </div>
+                <p className='pt-4'>するかここにドラッグ&ドロップ</p>
+                <p className='text-xs leading-5 text-gray-600'>PNG, JPG, GIF ファイル (最大10MB)</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='sm:col-span-3'>
+            <label htmlFor='latitude' className='block text-sm font-medium leading-6 text-gray-900'>
+              緯度
+            </label>
+            <div className='mt-2'>
+              <input type='number' step='0.01' name='latitude' value={formData.latitude} onChange={inputChange} className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
+            </div>
+          </div>
+
+          <div className='sm:col-span-3'>
+            <label htmlFor='longitude' className='block text-sm font-medium leading-6 text-gray-900'>
+              経度
+            </label>
+            <div className='mt-2'>
+              <input type='number' step='0.01' name='longitude' value={formData.longitude} onChange={inputChange} className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-6 flex items-center justify-end gap-x-6'>
+        <button type='submit' className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+          作成する
+        </button>
+      </div>
+    </form>
+  )
+}
+
+PointForm.propTypes = {
+  children: PropTypes.node.isRequired,
+  openPointForm: PropTypes.bool.isRequired,
+  setOpenPointForm: PropTypes.func.isRequired,
+}
+
+export default function PointForm({ children, openPointForm, setOpenPointForm }) {
+  return (
+    <Transition.Root show={openPointForm} as={Fragment}>
+      <Dialog as='div' className='relative z-10' onClose={setOpenPointForm}>
+        <Transition.Child as={Fragment} enter='ease-in-out duration-500' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in-out duration-500' leaveFrom='opacity-100' leaveTo='opacity-0'>
           <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
         </Transition.Child>
-
-        <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
-          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
-            <Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95' enterTo='opacity-100 translate-y-0 sm:scale-100' leave='ease-in duration-200' leaveFrom='opacity-100 translate-y-0 sm:scale-100' leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'>
-              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
-                <form onSubmit={onSubmit}>
-                  <div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
-                    <div className='sm:flex sm:items-start'>
-                      <div className='mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
-                        <div className='sm:col-span-3'>
-                          <label htmlFor='latitude' className='block text-sm font-medium leading-6 text-gray-900'>
-                            緯度
-                          </label>
-                          <div className='mt-2'>
-                            <input type='text' name='latitude' value={formData.latitude} onChange={inputChange} className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
-                          </div>
-                        </div>
-
-                        <div className='sm:col-span-3'>
-                          <label htmlFor='longitude' className='block text-sm font-medium leading-6 text-gray-900'>
-                            経度
-                          </label>
-                          <div className='mt-2'>
-                            <input type='text' name='longitude' value={formData.longitude} onChange={inputChange} className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
-                          </div>
-                        </div>
-
-                        <div className='col-span-full'>
-                          <label className='block text-sm font-medium leading-6 text-gray-900'>
-                            マーカー画像
-                            {formData.markerImage ? (
-                              <img
-                                // style={{ objectFit:'cover',width: 50, height: 50, display: 'block', border: 'none', borderRadius: '50%', cursor: 'pointer', padding: 0 }}
-                                src={URL.createObjectURL(formData.markerImage)}
-                                alt={formData.markerImage.name}
-                                className='img-preview sticky h-12 w-12 rounded-full bg-fixed object-cover'
-                              />
-                            ) : (
-                              <CameraIcon className='h-12 w-12 text-gray-300' aria-hidden='true' />
-                            )}
-                          </label>
-                          <div className='mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10'>
-                            <div className='text-center'>
-                              <PhotoIcon className='mx-auto h-12 w-12 text-gray-300' aria-hidden='true' />
-                              <div className='mt-4 flex text-sm leading-6 text-gray-600'>
-                                <label htmlFor='markerImage' className='relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500'>
-                                  <span>画像ファイルをアップロード</span>
-                                  <input type='file' id='markerImage' name='markerImage' onChange={fileInputChange} className='sr-only' />
-                                </label>
-                                <p className='pl-1'>するかここにドラッグ&ドロップ</p>
-                              </div>
-                              <p className='text-xs leading-5 text-gray-600'>PNG, JPG, GIF ファイル (最大10MB)</p>
-                            </div>
-                          </div>
-                        </div>
-                        <div className='sm:col-span-6 sm:col-start-1'>
-                          <label htmlFor='title' className='block text-sm font-medium leading-6 text-gray-900'>
-                            タイトル
-                          </label>
-                          <div className='mt-2'>
-                            <input type='text' name='title' value={formData.title} onChange={inputChange} className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
-                          </div>
-                        </div>
-                      </div>
+        <div className='fixed inset-0 overflow-hidden'>
+          <div className='absolute inset-0 overflow-hidden'>
+            <div className='pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10'>
+              <Transition.Child as={Fragment} enter='transform transition ease-in-out duration-500 sm:duration-700' enterFrom='translate-x-full' enterTo='translate-x-0' leave='transform transition ease-in-out duration-500 sm:duration-700' leaveFrom='translate-x-0' leaveTo='translate-x-full'>
+                <Dialog.Panel className='pointer-events-auto relative w-screen max-w-md'>
+                  <Transition.Child as={Fragment} enter='ease-in-out duration-500' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in-out duration-500' leaveFrom='opacity-100' leaveTo='opacity-0'>
+                    <div className='absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4'>
+                      <button type='button' className='relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white' onClick={() => setOpenPointForm(false)}>
+                        <span className='absolute -inset-2.5' />
+                        <span className='sr-only'>パネルを閉じる</span>
+                        <XMarkIcon className='h-6 w-6' aria-hidden='true' />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  <div className='flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl'>
+                    <div className='px-4 sm:px-6'>
+                      <Dialog.Title className='text-base font-semibold leading-6 text-gray-900'>マーカーを作成</Dialog.Title>
+                    </div>
+                    <div className='relative mt-6 flex-1 px-4 sm:px-6'>
+                      {children}
                     </div>
                   </div>
-                  <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
-                    <button type='submit' className='inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 sm:ml-3 sm:w-auto'>
-                      作成
-                    </button>
-                    <button type='button' className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto' onClick={() => setOpen(false)} ref={cancelButtonRef}>
-                      キャンセル
-                    </button>
-                  </div>
-                </form>
-              </Dialog.Panel>
-            </Transition.Child>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
         </div>
       </Dialog>
--- src/views/FlightPointMap/index.jsx
+++ src/views/FlightPointMap/index.jsx
@@ -1,19 +1,16 @@
 import { Fragment, useState, useEffect } from 'react'
-import { ChevronDownIcon, PencilSquareIcon, XMarkIcon } from '@heroicons/react/20/solid'
-import { Menu, Transition } from '@headlessui/react'
-import { classNames } from '@/utils'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import {  PencilSquareIcon } from '@heroicons/react/20/solid'
 
 import MapBox from '@/components/mapbox-gl'
 import PointInfo from '@/views/FlightPointMap/PointInfo'
-import PointForm from '@/views/FlightPointMap/PointForm'
-import PointForm2 from '@/views/FlightPointMap/PointForm2'
+import PointForm, { PointFormInput } from '@/views/FlightPointMap/PointForm'
 
 export default function FlightPointMap() {
   const [editMode, setEditMode] = useState(false)
   const [selectPoint, setSelectPoint] = useState(null)
   const [markerInfo, setMarkerInfo] = useState(null)
 
-  const [open, setOpen] = useState(false)
   const [openPointForm, setOpenPointForm] = useState(false)
 
   const editAction = () => {
@@ -21,67 +18,36 @@ export default function FlightPointMap() {
     editMode && setSelectPoint(null)
   }
 
-  const [formData, setFormData] = useState({
-    latitude: 0.0,
-    longitude: 0.0,
-    title: '',
-    markerImage: null,
-  })
-
-  useEffect(() => {
-    if (!selectPoint) return
-    setFormData({ ...formData, latitude: selectPoint.latitude, longitude: selectPoint.longitude })
-  }, [selectPoint])
-
   return (
     <div className='bg-white px-4 lg:px-0'>
       <div className='lg:flex lg:items-center lg:justify-between'>
         <PointInfo editMode={editMode} markerInfo={markerInfo} />
         <div className='mt-3 flex lg:ml-4 lg:mt-0'>
-          <span className='hidden sm:block'>
-            {editMode ? (
-              <button type='button' className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto' onClick={() => editAction()}>
-                <XMarkIcon className='-ml-0.5 mr-1.5 h-5 w-5 text-white' aria-hidden='true' />
-                キャンセル
-              </button>
-            ) : (
-              <button type='button' className='inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={() => editAction()}>
-                <PencilSquareIcon className='-ml-0.5 mr-1.5 h-5 w-5 text-white' aria-hidden='true' />
-                新しいマーカーを登録
-              </button>
-            )}
-          </span>
-          {/* <span className='sm:ml-3'>
-            <button type='button' className='inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-              <CheckIcon className='-ml-0.5 mr-1.5 h-5 w-5' aria-hidden='true' />
-              公開する
+          <span className='sm:ml-3'>
+            <button type='button' className={`inline-flex items-center rounded-md bg-${editMode ? 'red' : 'indigo'}-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-${editMode ? 'red' : 'indigo'}-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-${editMode ? 'red' : 'indigo'}-600`} onClick={() => editAction()}>
+              {editMode ? (
+                <>
+                  <XMarkIcon className='-ml-0.5 mr-1.5 h-5 w-5 text-white' aria-hidden='true' />
+                  キャンセル
+                </>
+              ) : (
+                <>
+                  <PencilSquareIcon className='-ml-0.5 mr-1.5 h-5 w-5 text-white' aria-hidden='true' />
+                  新しいマーカーを登録
+                </>
+              )}
             </button>
-          </span> */}
-
-          <Menu as='div' className='relative ml-3 sm:hidden'>
-            <Menu.Button className='inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400'>
-              More
-              <ChevronDownIcon className='-mr-1 ml-1.5 h-5 w-5 text-gray-400' aria-hidden='true' />
-            </Menu.Button>
-            <Transition as={Fragment} enter='transition ease-out duration-200' enterFrom='transform opacity-0 scale-95' enterTo='transform opacity-100 scale-100' leave='transition ease-in duration-75' leaveFrom='transform opacity-100 scale-100' leaveTo='transform opacity-0 scale-95'>
-              <Menu.Items className='absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a href='#' className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                      Edit
-                    </a>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
-            </Transition>
-          </Menu>
+          </span>
         </div>
       </div>
+      
       <div className='mx-auto max-w-7xl py-3'>
         <MapBox editMode={editMode} selectPoint={selectPoint} setSelectPoint={setSelectPoint} markerInfo={markerInfo} setMarkerInfo={setMarkerInfo} setOpen={setOpenPointForm} />
       </div>
-      <PointForm open={open} setOpen={setOpen} formData={formData} setFormData={setFormData} />
-      <PointForm2 openPointForm={openPointForm} setOpenPointForm={setOpenPointForm} />
+
+      <PointForm openPointForm={openPointForm} setOpenPointForm={setOpenPointForm}>
+        <PointFormInput setOpenPointForm={setOpenPointForm} selectPoint={selectPoint} />
+      </PointForm>
     </div>
   )
 }
